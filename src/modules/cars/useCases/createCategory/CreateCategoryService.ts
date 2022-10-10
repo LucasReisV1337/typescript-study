3ee33cb41a69327c07ca0e75100aa14ac67db9cd
Lucas Reis Vasconcelos <lucasv@kofre.com.br>
2022-10-10 14:24:05 -0300

    add

--- src/modules/cars/useCases/createCategory/CreateCategoryService.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
@@ -6,13 +6,11 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-    constructor(private categoriesRepository: ICategoriesRepository) { 
-
-    }
-
+    constructor(private categoriesRepository: ICategoriesRepository) {}
 
     execute({ description, name }: IRequest): void {
-        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+        const categoryAlreadyExists =
+            this.categoriesRepository.findByName(name);
 
         if (categoryAlreadyExists) {
             throw new Error("Category already exists!");
