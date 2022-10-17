f12ee7af6b1ca0b88a005bcd1d0f24d41e35e677
lucas <lucasreisvasconcelos@icloud.com>
2022-10-17 17:36:00 -0300

    add

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerUi from "swagger-ui-express"
+import { router } from "./routes";
+
+
+import swaggerFile from "./swagger.json"
 
 
 const app = express();
 
 const port = 1337;
 
-app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+
+
+app.use(express.json());
+app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(port, () =>
     console.log(
